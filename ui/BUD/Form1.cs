d8b8f4fdb34519423e9cea5dbace7ff426db19f6
Miguel Vila <miguelovila@ua.pt>
2024-05-29 01:59:06 +0100

    [NEW] Basic database integration

--- ui/BUD/Form1.cs
+++ ui/BUD/Form1.cs
@@ -20,7 +20,31 @@ namespace BUD
 
         private void btnAuthenticate_Click(object sender, EventArgs e)
         {
-            
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            if (username == "" || password == "")
+            {
+                MessageBox.Show("Please enter a username and password.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            using (SqlConnection connection = Database.GetDatabase().GetConnection())
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT * FROM BUD.[user]";
+                    connection.Open();
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine(reader["email"].ToString());
+                        }
+                    }
+                }
+            }
         }
     }
 }
