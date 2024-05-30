1299b791d577b714a9da38e18ed86ed1e16cb7a4
Miguel Vila <miguelovila@ua.pt>
2024-05-30 11:50:04 +0100

    [NEW] New View for Services Categories and Fields

--- db/Views.sql
+++ db/Views.sql
@@ -4,6 +4,9 @@ GO
 IF OBJECT_ID('BUD.UserInfo', 'V') IS NOT NULL DROP VIEW BUD.UserInfo;
 GO
 
+IF OBJECT_ID('BUD.ServiceCategoriesFields', 'V') IS NOT NULL DROP VIEW BUD.ServiceCategoriesFields;
+GO
+
 CREATE VIEW BUD.UserInfo AS
 SELECT 
     u.id AS user_id,
@@ -29,4 +32,25 @@ LEFT JOIN
     BUD.UserRoles ur ON u.id = ur.user_id
 LEFT JOIN 
     BUD.roles r ON ur.role_id = r.id;
+GO
+
+CREATE VIEW BUD.ServiceCategoriesFields AS
+SELECT
+    s.id AS service_id,
+    s.name AS service_name,
+    s.description AS service_description,
+    c.id AS category_id,
+    c.name AS category_name,
+    c.description AS category_description,
+    c.minimum_role AS category_minimum_role,
+    f.id AS field_id,
+    f.name AS field_name
+FROM
+    BUD.[service] s
+JOIN
+    BUD.category c ON s.id = c.service_id
+JOIN
+    BUD.category_field cf ON c.id = cf.category_id
+JOIN
+    BUD.field f ON cf.field_id = f.id;
 GO
